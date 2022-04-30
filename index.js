97a97c232c43c7b0a7b64b8c85cf21d3742e0be8
Rakib-Dev <rhbappy456@gmail.com>
2022-04-30 12:13:16 +0600

    bug fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 const app = express();
 
